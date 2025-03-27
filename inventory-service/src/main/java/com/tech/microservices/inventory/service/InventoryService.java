b869815e7d2dae40cb196243f5a5122c6056437f
123-Aditya <mraditya345@gmail.com>
2025-03-27 21:46:03 +0530

    Added logging in inventory service

--- inventory-service/src/main/java/com/tech/microservices/inventory/service/InventoryService.java
+++ inventory-service/src/main/java/com/tech/microservices/inventory/service/InventoryService.java
@@ -1,17 +1,22 @@
 package com.tech.microservices.inventory.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.tech.microservices.inventory.repository.InventoryRepository;
 
 @Service
 public class InventoryService {
+	
+	Logger log = LoggerFactory.getLogger(InventoryService.class);
 
 	@Autowired
 	private InventoryRepository inventoryRepository;
 
 	public boolean isInStock(String skuCode, Integer quantity) {
 		// Find an inventory for a given skuCode where quantity >= 0
+		log.info("Fetching product information from Inventory...");
 		return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode ,quantity);
 	}
 }
